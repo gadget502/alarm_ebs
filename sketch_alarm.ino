f7ba402aa98b21edfaadd9496c044827113fa06c
chicken2039 <zoq2039@gmail.com>
2019-09-27 19:46:15 +0900

    Poor is edited

--- sketch_alarm.ino
+++ sketch_alarm.ino
@@ -12,21 +12,17 @@ const int IO_PIN   = 8;   // Input/Output : DAT
 const int SCK_PIN = 7;   // Serial Clock : CLK
 const int pirPin = 39;
 int pirStat = 0;
-int move_cnt = 0;
-int total_cnt = 0;
-uint8_t alarm_time[4]= {25,61,61,8};
-uint8_t alarm_LED[4]={255,255,255,10};
-uint8_t alarm_DoW[7]={0,0,0,0,0,0,0};
-int volume = 0x30;
+float move_cnt = 0.0f;
+float total_cnt = 0.0f;
+uint8_t alarm_LED[3] = {255, 255, 255};
+byte alarm_volume = 0x30;
 
+boolean isAlarmStarted = false;
 
-//네오픽셀을 사용하기 위해 객체 하나를 생성한다. 
+//네오픽셀을 사용하기 위해 객체 하나를 생성한다.
 //첫번째 인자값은 네오픽셀의 LED의 개수
 //두번째 인자값은 네오픽셀이 연결된 아두이노의 핀번호
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, 25, NEO_GRB + NEO_KHZ800);
-
-//  DS1302 오브젝트 생성
-virtuabotixRTC myRTC(13, 5, 2);
 
 // Constants
 const char* ssid = "MisakaMOE";
@@ -39,89 +35,123 @@ WiFiClient client;
 WebSocketClient webSocketClient;
 
 void setup() {
+  colorWipe(strip.Color(255  , 255 , 255), 10);
+  //Initialize LED strip
+  strip.begin(); //네오픽셀을 초기화하기 위해 모든LED를 off시킨다
+  strip.show();
+  //Initialize Serial
   Serial.begin(9600);
-  //myRTC.setDS1302Time(00, 46, 20, 2, 26, 8, 2019);
+  //Initialize Wifi
   WiFi.begin(ssid, password);
   while ( WiFi.status() != WL_CONNECTED ) {
     delay(500);
-//    Serial.print(".");
-  pinMode(pirPin , INPUT); // pirPin 초기화
   }
-  
-// Print our IP address
-// Serial.println("Connected!");
-//  Serial.print("My IP address: ");
-//  Serial.println(WiFi.localIP());
-  
-  if(client.connect("192.168.0.17",80)){
-//      Serial.println("Connected");
-    } else {
-//      Serial.println("Connection failed");
-    }
-    
-  webSocketClient.path=path;
-  webSocketClient.host=host;
+  pinMode(pirPin , INPUT); // pirPin 초기화
+  //Connect to Wifi
+  client.connect("192.168.0.17", 80);
+  //Initialize WebSocket
+  webSocketClient.path = path;
+  webSocketClient.host = host;
+  //Initialize MP3 Module
+  dfpExecute(0x3F, 0x00, 0x00);
+  while(Serial.available()<10)
+    delay(30);
 
-  strip.begin(); //네오픽셀을 초기화하기 위해 모든LED를 off시킨다
-  strip.show();
 }
 
 
 void loop() {
   String data;
-  String g_data;
-  myRTC.updateTime(); 
- //if (!(myRTC.seconds % 10)){
-    pirStat = digitalRead(pirPin); 
-    if (pirStat == HIGH) {   // if motion detected
-      move_cnt++;
-        colorWipe(strip.Color(0 ,0 , 255), 10); 
-      
-    }
-    else{
-        colorWipe(strip.Color(0 ,255 , 0), 10);  
-    }  
-    total_cnt++;
-   
-    if((myRTC.minutes % 10 == 0) && (myRTC.seconds % 60 == 0)){
-      data =String(move_cnt/total_cnt);
-      //webSocketClient.sendData(data);
-      move_cnt = 0;
-      total_cnt = 0;
-      }
-    if((myRTC.hours==alarm_time[0])&&(myRTC.minutes==alarm_time[1])&&(myRTC.seconds==alarm_time[2])&&(alarm_DoW[myRTC.dayofweek-1]))//서버에서 받아오는 월처리
+
+  if(client.connected())
+  {
+    colorWipe(strip.Color(255 , 0 , 0), 10); //connected Visualize
+    webSocketClient.getData(data);
+    if(data.length() > 0)
     {
-      alarm_start(0x30);
-      
-    } 
-    webSocketClient.getData(g_data);
-    
-    if (data.length() > 0) {
-      if(1){
-        //알람 설정
-        alarm_set(data);
-        }
-      else if(1)
-       alarm_end();
-        //버튼을 이용한 알람 종료
-      
+      //Compare Header, then run proper methods for each header
+      if("LED")
+      {
+        String Red,Green,Blue;
+        int dR,dG,dB;
+        webSocketClient.getData(Red);
+        webSocketClient.getData(Green);
+        webSocketClient.getData(Blue);
+        alarm_LED[0]= Green.toInt();
+        alarm_LED[1]= Red.toInt();
+        alarm_LED[2]= Blue.toInt();
+            
+      }
+      else if("AlarmStart"){
+        startAlarm(alarm_volume,alarm_LED[0],alarm_LED[1],alarm_LED[2]);
+      }
+      else if("AlarmStop"){
+        
+        stopAlarm();
+      }
+      else if ("CheckStart"){
+        isAlarmStarted = true;
+        
+      }
+       else if ("CheckStop"){
+        isAlarmStarted = false;
+        
+      }
+      else if("MusicVolume"){
+        String volume = "";
+        webSocketClient.getData(volume);
+        alarm_volume = volume.toInt();
+        setVolume();
+      }
     }
     else {
+      //state check -> if alarm started, then check movement
+      if(isAlarmStarted){
+        recordMovement();
+      }
     }
- 
-  delay(1000);
-
+  }
+  else
+  {
+    colorWipe(strip.Color(0 , 255 , 0), 10);
+    //Client disconnected : Try to reconnect wi-fi
+    while ( WiFi.status() != WL_CONNECTED ) {
+      delay(500);
+    }
+   //Connect to Wifi
+   client.connect("192.168.0.17", 80);//disconnected Visualize
+    
+  }
+  
 }
 
 
+void recordMovement()
+{
+  pirStat = digitalRead(pirPin);
+  if (pirStat == HIGH) {   // if motion detected
+    move_cnt++;
+    colorWipe(strip.Color(0 , 0 , 255), 10);
+  }
+  else {
+    colorWipe(strip.Color(0 , 255 , 0), 10);
+  }
+  total_cnt++;
 
+  if (total_cnt>=60) {
+    webSocketClient.sendData(String((move_cnt*1.0) / total_cnt));
+    move_cnt = 0.0;
+    total_cnt = 0.0;
+  }
+  delay(910);
+}
 
 //NeoPixel에 달린 LED를 각각 주어진 인자값 색으로 채워나가는 함수
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
   }
 }
 
@@ -129,9 +159,9 @@ void colorWipe(uint32_t c, uint8_t wait) {
 void rainbow(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i + j) & 255));
     }
     strip.show();
     delay(wait);
@@ -142,8 +172,8 @@ void rainbow(uint8_t wait) {
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
 
-  for(j=0; j<256*5; j++) { 
-    for(i=0; i< strip.numPixels(); i++) {
+  for (j = 0; j < 256 * 5; j++) {
+    for (i = 0; i < strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
@@ -153,17 +183,17 @@ void rainbowCycle(uint8_t wait) {
 
 //입력한 색으로 LED를 깜빡거리며 표현한다
 void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+  for (int j = 0; j < 10; j++) { //do 10 cycles of chasing
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, c);  //turn every third pixel on
       }
       strip.show();
-     
+
       delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);      //turn every third pixel off
       }
     }
   }
@@ -171,60 +201,67 @@ void theaterChase(uint32_t c, uint8_t wait) {
 
 //LED를 다양한색으로 표현하며 깜빡거린다
 void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     //256가지의 색을 표현
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0); 
-        }
+  for (int j = 0; j < 256; j++) {   //256가지의 색을 표현
+    for (int q = 0; q < 3; q++) {
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, Wheel( (i + j) % 255));
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (int i = 0; i < strip.numPixels(); i = i + 3) {
+        strip.setPixelColor(i + q, 0);
+      }
     }
   }
 }
 
 //255가지의 색을 나타내는 함수
 uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  if (WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
 }
 
-void dfpExecute(byte CMD, byte Par1, byte Par2)  
-{   
-  # define Start_Byte   0x7E  
-  # define Version_Byte  0xFF  
-  # define Command_Length 0x06  
-  # define Acknowledge  0x00   
-  # define End_Byte    0xEF  
-  // Calculate the checksum (2 bytes)  
-  uint16_t checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2);  
-  // Build the command line  
-  uint8_t Command_line[10] = { Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,  
-         Par1, Par2, highByte(checksum), lowByte(checksum), End_Byte};  
-  // Send the command line to DFPlayer  
-  for (byte i=0; i<10; i++) Serial.write(Command_line[i]);  
+void dfpExecute(byte CMD, byte Par1, byte Par2)
+{
+# define Start_Byte   0x7E
+# define Version_Byte  0xFF
+# define Command_Length 0x06
+# define Acknowledge  0x00
+# define End_Byte    0xEF
+  // Calculate the checksum (2 bytes)
+  uint16_t checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2);
+  // Build the command line
+  uint8_t Command_line[10] = { Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,
+                               Par1, Par2, highByte(checksum), lowByte(checksum), End_Byte
+                             };
+  // Send the command line to DFPlayer
+  for (byte i = 0; i < 10; i++) Serial.write(Command_line[i]);
 }
-void alarm_start(byte vol){
-  dfpExecute(0x06,vol,0x00); // VolumeSet
-  dfpExecute(0x11,0x01,0x00); //repeatset
-  dfpExecute(0x0F,0x01,0x01); //PlayMusic
+
+void startAlarm(byte vol,uint8_t g,uint8_t r,uint8_t b) {
+  colorWipe(strip.Color(g , r , b), 10);
+  dfpExecute(0x06, vol, 0x00); // VolumeSet
+  dfpExecute(0x11, 0x01, 0x00); //repeatset
+  dfpExecute(0x0F, 0x01, 0x01); //PlayMusic
+  delay(30);
 }
-void alarm_end(){
-  dfpExecute(0x0E,0x00,0x00);//Pause(End)
-  colorWipe(strip.Color(0 ,0 , 0), 0);
+
+void setVolume()
+{
+    dfpExecute(0x06, alarm_volume, 0x00); // VolumeSet
 }
-void alarm_set(String socket){
-  //socket set
-  
-  }
+
+void stopAlarm() {
+  dfpExecute(0x0E, 0x00, 0x00); //Pause(End)
+  colorWipe(strip.Color(0 , 0 , 0), 0);
+  isAlarmStarted = false;
+}
